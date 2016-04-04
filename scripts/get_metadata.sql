d7ecf759a145c77e05d0a60418e445f96799a097
Taryn Heilman <tarynheilman@gmail.com>
2016-04-04 13:18:19 -0600

    SQL query used to get metadata

--- scripts/get_metadata.sql
+++ scripts/get_metadata.sql
@@ -3,3 +3,11 @@
 SELECT
 ra, dec, nvote, p_el, p_cw, p_acw, p_edge, p_dk, p_mg, p_cs
 FROM ZooNoSpec
+
+# RA, DEC, number of votes, (fraction of votes for:) elliptical, clockwise spiral,
+# edge-on disk, don't know, merger, combined spiral
+
+
+SELECT
+ra, dec, nvote, p_el, p_cw, p_acw, p_edge, p_dk, p_mg, p_cs
+FROM ZooSpec
