9c77f8cf010278fa4304a46b26126442d9fb9c50
Ubuntu <ubuntu@ip-172-31-59-99.ec2.internal>
2016-04-11 21:48:39 +0000

    very promising iteration of net on testing set

--- scripts/CNN_test.py
+++ scripts/CNN_test.py
@@ -46,12 +46,12 @@ def convert_targets(targets):
     output: targets dummified category matrix
     note: targets are indexed as ['elliptical', 'merger', 'spiral']
     '''
-    # return pd.get_dummies(targets).values
+    return pd.get_dummies(targets).values
     # because I'm running the data on only spirals...
-    np_utils.to_categorical(pd.get_dummies(y_test).values, 3)
+    # np_utils.to_categorical(pd.get_dummies(y_test).values, 3)
 
 
-def nn_model(X_train, y_train, X_test, y_test, batch_size = 100, nb_classes = 3, nb_epoch = 1):
+def nn_model(X_train, y_train, X_test, y_test, batch_size = 100, nb_classes = 4, nb_epoch = 30):
     # need to fix docs for X_train and X_test as these should be 3D or 4D arrays
     '''
     input: X_train (4D np array), y_train (1D np array), X_test (4D np array), y_test (1D np array)
@@ -76,7 +76,6 @@ def nn_model(X_train, y_train, X_test, y_test, batch_size = 100, nb_classes = 3,
     model = Sequential()
 
     # first convolutional layer and subsequent pooling
-    # model.add(Convolution2D(32, 1, 1, border_mode='valid', input_shape=(60, 60, 3), activation='relu', dim_ordering='tf', subsample=(1, 1)))
     model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(60, 60, 3), activation='relu', dim_ordering='tf', init='normal'))
     # model.add(MaxPooling2D(pool_size=(2, 2)))
     # model.save_weights('weights1_2')
@@ -115,7 +114,7 @@ def nn_model(X_train, y_train, X_test, y_test, batch_size = 100, nb_classes = 3,
     # model.save_weights('weights_6_7')
 
     # output layer
-    model.add(Dense(3, init='normal'))
+    model.add(Dense(4, init='normal'))
     model.add(Activation('softmax'))
     # model.save_weights('end_weights')
 
@@ -141,7 +140,7 @@ def scores(model, X_test, y_test):
 
 
 if __name__ == '__main__':
-    files = ['X_train_spirals.npy', 'X_test_spirals.npy', 'y_train_spirals.npy', 'y_test_spirals.npy']
+    files = ['X_train_small.npy', 'X_test_small.npy', 'y_train_small.npy', 'y_test_small.npy']
     X_train, X_test, y_train, y_test = (np.load(path_to_project_data+file) for file in files)
     # np.random.seed(18)  # for reproducibility
 
